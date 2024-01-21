9dc9fd7fe690476b3ba65cc5c8b17deee51d6744
Mr-Mic <bhatade.2114501587@mujonline.edu.in>
2024-01-21 11:09:00 +0530

    Tailwind_and_Props

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCounter] = useState(0);
+
+  const addOne = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+    setCounter(counter - 1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>TailwIND Test</h1>
+      <p>{counter}</p>
+      <button onClick={addOne}>+</button>
+      <button onClick={removeValue}>-</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
