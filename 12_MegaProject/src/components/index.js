6e29a0fc15cf8e67a164c9c128688c115d088a1f
Mr-Mic <bhatade.2114501587@mujonline.edu.in>
2024-02-18 12:25:20 +0530

    react-forms features

--- 12_MegaProject/src/components/index.js
+++ 12_MegaProject/src/components/index.js
@@ -5,5 +5,21 @@ import Input from './Input';
 import Logo from './Logo';
 import Button from './Button';
 import LogoutBtn from './Header/LogoutBtn';
+import PostCard from './PostCard';
+import Select from './Select';
+import Login from './Login';
+import SignUp from './SignUp';
 
-export { Header, Footer, Container, Input, Logo, Button, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Container,
+  Input,
+  Logo,
+  Button,
+  LogoutBtn,
+  PostCard,
+  Select,
+  Login,
+  SignUp,
+};
