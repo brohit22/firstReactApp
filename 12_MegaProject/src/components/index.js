af093fd4550043eb5c66afc9ac8f8ac1fb71fb7d
Mr-Mic <bhatade.2114501587@mujonline.edu.in>
2024-02-18 21:42:19 +0530

    Page strcture done

--- 12_MegaProject/src/components/index.js
+++ 12_MegaProject/src/components/index.js
@@ -6,10 +6,12 @@ import Logo from './Logo';
 import Button from './Button';
 import LogoutBtn from './Header/LogoutBtn';
 import PostCard from './PostCard';
+import PostForm from './Post/PostForm';
 import Select from './Select';
 import Login from './Login';
 import SignUp from './SignUp';
 import RTE from './RTE';
+import AuthLayout from './AuthLayout';
 
 export {
   Header,
@@ -20,8 +22,10 @@ export {
   Button,
   LogoutBtn,
   PostCard,
+  PostForm,
   Select,
   Login,
   SignUp,
   RTE,
+  AuthLayout,
 };
--- 12_MegaProject/src/main.jsx
+++ 12_MegaProject/src/main.jsx
@@ -5,12 +5,67 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+import { AuthLayout } from './components/index.js';
+import Home from './pages/Home.jsx';
+import Signup from './pages/Signup.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [],
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/all-posts',
+        element: (
+          <AuthLayout authentication>
+            {' '}
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/add-post',
+        element: (
+          <AuthLayout authentication>
+            {' '}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/edit-post/:slug',
+        element: (
+          <AuthLayout authentication>
+            {' '}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/post/:slug',
+        element: <Post />,
+      },
+    ],
   },
 ]);
 
